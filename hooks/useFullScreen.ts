e4a5259b1259413845495f68370c5f42112d5b8a
tecnoboyer <ingenieroboyer@gmail.com>
2025-06-24 12:24:42 -0400

    functional

--- hooks/useFullScreen.ts
+++ hooks/useFullScreen.ts
@@ -1,16 +1,16 @@
-// // hooks/useFullScreen.ts
-// import * as NavigationBar from 'expo-navigation-bar';
-// import * as StatusBar from 'expo-status-bar';
-// import { useEffect } from 'react';
-// import { Platform } from 'react-native';
+// hooks/useFullScreen.ts
+import * as NavigationBar from 'expo-navigation-bar';
+import * as StatusBar from 'expo-status-bar';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 
-// export function useFullScreen() {
-//   useEffect(() => {
-//     if (Platform.OS === 'android') {
-//       NavigationBar.setVisibilityAsync('hidden');
-//       NavigationBar.setBehaviorAsync('inset-swipe');
-//     }
+export function useFullScreen() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setVisibilityAsync('hidden');
+      NavigationBar.setBehaviorAsync('inset-swipe');
+    }
 
-//     StatusBar.setStatusBarHidden(true, 'slide');
-//   }, []);
-// }
+    StatusBar.setStatusBarHidden(true, 'slide');
+  }, []);
+}
